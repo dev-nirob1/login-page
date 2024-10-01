65d6bd9e7019e8935e6262256f21e69e05412653
dev-nirob1 <dev-nirob1@gmail.com>
2024-10-01 15:35:46 +0600

    login page

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx,html}', // Ensure you include the correct paths to your files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
